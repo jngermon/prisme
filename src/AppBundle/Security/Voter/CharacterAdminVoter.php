c970ee7ac1c0f303060985855e62647a60d8e4f9
jngermon <jn.germon@gmail.com>
2017-12-09 23:49:29 +0100

    Create voter for character admin

---
+++ src/AppBundle/Security/Voter/CharacterAdminVoter.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace AppBundle\Security\Voter;
+
+use AppBundle\Admin\CharacterAdmin;
+use AppBundle\Entity\Character;
+use AppBundle\Entity\Organizer;
+use AppBundle\Entity\Player;
+use AppBundle\Entity\User;
+use AppBundle\Security\ProfileProvider;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class CharacterAdminVoter extends Voter
+{
+    protected $rolePattern;
+
+    protected $decisionManager;
+
+    protected $profileProvider;
+
+    public function __construct(
+        AccessDecisionManagerInterface $decisionManager,
+        ProfileProvider $profileProvider
+    ) {
+        $this->decisionManager = $decisionManager;
+        $this->profileProvider = $profileProvider;
+
+        $this->rolePattern = '/^ROLE_APP_ADMIN_CHARACTER_(.*)$/';
+    }
+
+    protected function supports($attribute, $subject)
+    {
+        return preg_match($this->rolePattern, $attribute);
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        if ($this->decisionManager->decide($token, array('ROLE_SUPER_ADMIN'))) {
+            return true;
+        }
+
+        if (!$token->getUser() instanceof User) {
+            return false;
+        }
+
+        $character = null;
+        if ($subject && $subject instanceof CharacterAdmin) {
+            $character = $subject->getSubject();
+        } elseif ($subject && $subject instanceof Character) {
+            $character = $subject;
+        }
+
+        preg_match($this->rolePattern, $attribute, $matches);
+
+        $attribute = $matches[1];
+
+        switch ($attribute) {
+            case 'LIST':
+                return $this->canList($token->getUser());
+            case 'CREATE':
+                return $this->canCreate($token->getUser());
+            case 'VIEW':
+                return $character && $this->canView($character, $token->getUser());
+            case 'EDIT':
+                return $character && $this->canEdit($character, $token->getUser());
+        }
+
+        return false;
+    }
+
+
+    protected function canList(User $user)
+    {
+        if (!$this->profileProvider->getActiveProfile()) {
+            return false;
+        }
+
+        return $this->profileProvider->getActiveProfile() instanceof Organizer;
+    }
+
+    protected function canCreate(User $user)
+    {
+        if (!$this->profileProvider->getActiveProfile()) {
+            return false;
+        }
+
+        return $this->profileProvider->getActiveProfile() instanceof Organizer;
+    }
+
+    protected function canView(Character $character, User $user)
+    {
+        if (!$character->getId()) {
+            return true;
+        }
+
+        $profile = $this->profileProvider->getActiveProfile();
+
+        if (!$profile) {
+            return false;
+        }
+
+        if ($profile instanceof Organizer && $profile->getLarp() == $character->getLarp()) {
+            return true;
+        }
+
+        if ($profile instanceof Player && $profile == $character->getPalyer()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function canEdit(Character $character, User $user)
+    {
+        $profile = $this->profileProvider->getActiveProfile();
+
+        if (!$profile) {
+            return false;
+        }
+
+        if ($profile instanceof Organizer && $profile->getLarp() == $character->getLarp()) {
+            return true;
+        }
+
+        return false;
+    }
+}
