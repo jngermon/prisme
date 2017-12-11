76410d96058971388de53b32313a67b866bd174e
jngermon <jn.germon@gmail.com>
2017-12-11 20:14:14 +0100

    Rename index in migration to avoid duplicate name

--- app/DoctrineMigrations/Version20171209232337.php
+++ app/DoctrineMigrations/Version20171209232337.php
@@ -15,7 +15,10 @@ class Version20171209232337 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE UNIQUE INDEX larp_person_idx ON organizer (larp_id, person_id)');
+        $this->addSql('CREATE UNIQUE INDEX organizer_larp_person_idx ON organizer (larp_id, person_id)');
+
+        $this->addSql('DROP INDEX larp_person_idx');
+        $this->addSql('CREATE UNIQUE INDEX player_larp_person_idx ON player (larp_id, person_id)');
     }
 
     public function down(Schema $schema)
@@ -23,6 +26,8 @@ class Version20171209232337 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP INDEX larp_person_idx');
+        $this->addSql('DROP INDEX organizer_larp_person_idx');
+        $this->addSql('DROP INDEX player_larp_person_idx');
+        $this->addSql('CREATE UNIQUE INDEX larp_person_idx ON player (larp_id, person_id)');
     }
 }
