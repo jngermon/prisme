9249ef7f94918ddf708003777468ab3012ffccab
jngermon <jn.germon@gmail.com>
2018-02-05 22:29:55 +0100

    Fix bad use of inline javascript

--- src/AppBundle/Resources/assets/js/app.js
+++ src/AppBundle/Resources/assets/js/app.js
@@ -2,5 +2,10 @@ require('./vendor');
 require('./widget/calendar_date');
 
 $(document).ready(function() {
+
+    $('.sonata_dtp').each(function(idx, dtp){
+        $(dtp).datetimepicker($(dtp).data('options'));
+    });
+
     $('.calendar_date').calendarDate();
 });
